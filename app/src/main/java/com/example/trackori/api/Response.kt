78f6e8b12e4118bdf2a75562ae2c46c20f03d7fc
Bintang Nursyawalli <bintang.nursyawalli@ui.ac.id>
2023-05-29 18:59:48 +0700

    Implement the API

--- app/src/main/java/com/example/trackori/api/Response.kt
+++ app/src/main/java/com/example/trackori/api/Response.kt
@@ -7,5 +7,85 @@ data class LoginCredentials(
 data class RegisterCredentials(
     val username: String,
     val email: String,
-    val password: String
+    val password: String,
+    val gender : String,
+    val weight : Float,
+    val height : Float,
+    val plan : String?,
+)
+
+data class RegisterResponse(
+    val error: Boolean,
+    val message: String
+)
+
+// Additional Models
+
+data class UserResponse(
+    val success: Boolean,
+    val message: String,
+    val data: User
+)
+
+data class User(
+    val uid: String,
+    val email: String,
+    val username: String,
+    val gender: String,
+    val age: Int,
+    val weight: Float,
+    val height: Float,
+    val dailyCalorieNeeds: Float? = null, // Nullable as it's optional
+    val plan: String? = null // Nullable as it's optional
+)
+
+data class CalorieHistoryItem(
+    val id: String,
+    val calories: Float,
+    val date: String
+)
+
+data class CalorieHistoryResponse(
+    val success: Boolean,
+    val message: String,
+    val data: List<CalorieHistoryItem>
+)
+
+data class EditCredentials(
+    val email: String?,
+    val password: String?,
+    val currentEmail: String,
+    val currentPassword: String
+)
+
+data class EditCredentialsResponse(
+    val success: Boolean,
+    val message: String,
+    val data: User
+)
+
+data class ResetPasswordCredentials(
+    val email: String
+)
+
+data class ResetPasswordResponse(
+    val success: Boolean,
+    val message: String,
+    val data: ResetPasswordCredentials
+)
+
+data class LogoutResponse(
+    val success: Boolean,
+    val message: String
+)
+
+// For data in calorie-history
+data class CalorieHistoryData(
+    val calories: Float
+)
+
+data class CalorieHistoryDataResponse(
+    val success: Boolean,
+    val message: String,
+    val data: CalorieHistoryData
 )
---
+++ app/src/main/java/com/example/trackori/api/TrackoriApi.kt
@@ -0,0 +1,44 @@
+package com.example.trackori.api
+
+import retrofit2.Call
+import retrofit2.http.*
+
+interface TrackoriApi {
+
+    @POST("register")
+    fun register(@Body user: RegisterCredentials): Call<RegisterResponse>
+
+    @POST("login")
+    fun login(@Body credentials: LoginCredentials): Call<UserResponse>
+
+    @GET("user{uid}")
+    fun getUserInfo(@Path("uid") uid: String): Call<UserResponse>
+
+    @POST("users/{uid}/calorie-history")
+    fun addCalorieHistory(@Path("uid") uid: String, @Body data: CalorieHistoryData): Call<CalorieHistoryDataResponse>
+
+    @GET("users/{uid}/get-calorie-history")
+    fun getCalorieHistoryByDate(@Path("uid") uid: String, @Query("date") date: String): Call<CalorieHistoryResponse>
+
+    @GET("users/{uid}/all-calorie-history")
+    fun getAllCalorieHistory(@Path("uid") uid: String): Call<CalorieHistoryResponse>
+
+    @PUT("users/{uid}/calorie-history/{docId}")
+    fun editCalorieHistory(@Path("uid") uid: String, @Path("docId") docId: String, @Body data: CalorieHistoryData): Call<CalorieHistoryDataResponse>
+
+    @PUT("edit-info/{uid}")
+    fun editUserInfo(@Path("uid") uid: String, @Body user: User): Call<UserResponse>
+
+    @PUT("edit-credential/{uid}")
+    fun editUserCredentials(@Path("uid") uid: String, @Body credentials: EditCredentials): Call<EditCredentialsResponse>
+
+    @POST("reset-password")
+    fun resetPassword(@Body credentials: ResetPasswordCredentials): Call<ResetPasswordResponse>
+
+    @POST("logout")
+    fun logout(): Call<LogoutResponse>
+
+    // For the protected resources
+    @GET("protected")
+    fun accessProtected(@Header("Authorization") token: String): Call<UserResponse>
+}
